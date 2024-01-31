02594672f24fcf68b878eab696289afda1b62e2a
betofs1 <betofs1@live.com>
2024-01-31 11:48:45 -0300

    teste

--- sentenca_1.js
+++ sentenca_1.js
@@ -1,13 +1,7 @@
-const produto1 = {}
-produto1.nome = 'iphone'
-produto1.valor = 5000.10
-produto1 ['desconto massa'] = 0.40
-
-console.log(produto1)
-
-const produto2 = {
-  nome:'iphone',
-  preco: 4999.10
+let teste = 1
+{
+  let teste = 2
+  console.log("dentro é " + teste)
 }
 
-console.log(produto2)
+console.log("fora é " + teste)
