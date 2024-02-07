fbeeb6f04a4b941990c57d5add3331ea45781190
betofs1 <betofs1@live.com>
2024-02-07 14:53:05 -0300

    three bot

---
+++ sentenca_1.js
@@ -0,0 +1,13 @@
+const produto = {}
+produto.nome = "Iphone 13 PRO MAX"
+produto.valor = 5000
+
+console.log (produto)
+
+const produto2 = {
+  nome: "CAMISA",
+  preco: 500,
+}
+console.log (produto2)
+
+
